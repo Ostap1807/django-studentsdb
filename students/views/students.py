8d690109987d9f5be3717cd99139d4b0d772db93
Ostap Pankevych <flamaster1807@gmail.com>
2017-08-13 14:23:53 +0300

    added request context processor, changed structure of views files

--- students/views/students.py
+++ students/views/students.py
@@ -3,8 +3,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Views for Students
-
 def students_list(request):
     students = (
         {'id': 1,
@@ -35,16 +33,4 @@ def students_edit(request, sid):
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
 
-# Views for Groups
 
-def groups_list(request):
-    return render(request, 'students/groups_list.html', {})
-
-def groups_add(request):
-    return HttpResponse('<h1>Group Add Form</h1>')
-
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-
-def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
