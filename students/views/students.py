2d7acf1cf567442b0e9cb357f7ad278f9cde3b71
Ostap1807 <flamaster1807@gmail.com>
2017-10-08 12:13:29 +0300

    added validate of student form data

--- students/views/students.py
+++ students/views/students.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from datetime import datetime
 
 from ..models import Student, Group
 
@@ -40,20 +41,66 @@ def students_add(request):
         if request.POST.get('add_button') is not None:
             # errors collection
             errors = {}
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'),
+                    'notes': request.POST.get('notes')}
 
+            # validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u"Ім'я є обов'язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u"Прізвище є обов'язковим"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u"Дата народження є обов'язковою"
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] =\
+                        u"Введіть коректний формат дати (напр. 1984-12-30)"
+
+                else:
+                    data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u"Номер білета є обов'язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = u"Оберіть групу для студента"
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = u"Оберіть коректну групу"
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            if photo:
+                if photo.size > 2 * 1024 * 1024:
+                    errors['photo'] = \
+                        u"Розмір картинки не повинен перевищувати 2MB"
+                elif photo.content_type not in ['image/jpeg', 'image/jpg',
+                                              'image/png']:
+                    errors['photo'] = u"Недопустимий формат файлу"
+                else:
+                    data['photo'] = photo
+
+            # save student
             if not errors:
-                # create student object
-                student = Student(
-                    first_name=request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],
-                )
-
-                # save it to database
+                student = Student(**data)
                 student.save()
 
                 # redirect user to student list
@@ -62,8 +109,8 @@ def students_add(request):
             else:
                 # render form with errors and previous user input
                 return render(request, 'students/students_add.html',
-                               {'groups': Group.objects.all().order_by('title')},
-                               {'errors': errors})
+                               {'groups': Group.objects.all().order_by('title'),
+                                'errors': errors})
         elif request.POST.get('cancel_button') is not None:
             # redirect to home page on cancel button
             return HttpResponseRedirect(reverse('home'))
